1864e9f2b1aa0021857bba57acb097b6997d124e
caioclcc <caio_luiz28@hotmail.com>
2019-04-02 22:18:16 -0300

    att tela 2

--- app/src/main/java/br/com/etecia/login_app/MainActivity.java
+++ app/src/main/java/br/com/etecia/login_app/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.etecia.login_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txtUser = findViewById(R.id.txtUser);
+                TextView txtPass = findViewById(R.id.txtPass);
+                String user = txtUser.getText().toString();
+                String pass = txtPass.getText().toString();
+
+            }
+        });
     }
 }
